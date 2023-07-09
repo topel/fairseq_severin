82ba5b484f52e7bc5603a377b6c3470dedca8055
Séverin <123748182+SevKod@users.noreply.github.com>
2023-07-09 12:05:03 +0200

    Update dump_hubert_feature.py

--- examples/hubert/simple_kmeans/dump_hubert_feature.py
+++ examples/hubert/simple_kmeans/dump_hubert_feature.py
@@ -40,7 +40,6 @@ class HubertFeatureReader(object):
 
     def read_audio(self, path, ref_len=None):
         wav = get_features_or_waveform(path, need_waveform=True, use_sample_rate=self.task.cfg.sample_rate)
-        print(path)
         if wav.ndim == 2:
             wav = wav.mean(-1)
         assert wav.ndim == 1, wav.ndim
@@ -52,7 +51,6 @@ class HubertFeatureReader(object):
         x = self.read_audio(path, ref_len=ref_len)
         with torch.no_grad():
             x = torch.from_numpy(x).float().cuda()
-            #print(x.size())
             if self.task.cfg.normalize:
                 x = F.layer_norm(x, x.shape)
             x = x.view(1, -1)
@@ -60,7 +58,6 @@ class HubertFeatureReader(object):
             feat = []
             for start in range(0, x.size(1), self.max_chunk):
                 x_chunk = x[:, start : start + self.max_chunk]
-                print(x_chunk.size())
                 if (x_chunk.size(1) < 428):
                     length_to_pad = 428 - x_chunk.size(1)
                     zeros = torch.zeros((1,length_to_pad)).cuda()
